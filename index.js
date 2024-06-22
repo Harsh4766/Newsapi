69deb8c0239f413db60f7efdaf04b63624b7ba0d
Harsh4766 <yadavhd04@gmail.com>
2024-06-22 14:34:47 +0530

    Commit

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = 5000;
+const cors =require('cors');
 
 const NEWS_API_KEY = '0105ab8be5434126ad4e86c73e8a7a0e';
 const BASE_URL = 'https://newsapi.org/v2/top-headlines';
 
+app.use(cors());
 app.get('/news', async (req, res) => {
     const category = req.query.category;
     const country = 'in';
