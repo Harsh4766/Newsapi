422b6b40040bbf9b1fa514a32241b532d8aaa42c
Harsh Yadav <115269033+Harsh4766@users.noreply.github.com>
2024-08-21 23:42:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 5000;
 const cors =require('cors');
 
 const NEWS_API_KEY = '0105ab8be5434126ad4e86c73e8a7a0e';
-const BASE_URL = 'https://newsapi.org/v2/top-headlines';
+const BASE_URL = 'https://newsapi.org/v2/everything';
 
 app.use(cors());
 app.get('/news', async (req, res) => {
@@ -13,7 +13,7 @@ app.get('/news', async (req, res) => {
     const country = 'in';
 
     try {
-        const response = await axios.get(`${BASE_URL}?sources=techcrunch&apiKey=${NEWS_API_KEY}`);
+        const response = await axios.get(`${BASE_URL}?q=${category}&apiKey=${NEWS_API_KEY}`);
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch data from NewsAPI' });
